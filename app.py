bb6e2a840983e298a532d8d850da3572dcfb412b
Yutaro Matsuyama <matsuyamayutaro1@gmail.com>
2021-05-21 15:30:54 +0900

    pyできた

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import sqlite3, random
 app = Flask(__name__)
 app.secret_key = "sunabacokoza"
 
+@app.route("/")
+def main():
+    return render_template("main.html")
 
 @app.route("/gacha")
 def color():
@@ -12,11 +15,11 @@ def color():
     c.execute('SELECT "やること", "一言", "リンク" FROM "koumoku-t" WHERE "yaru-id" = 1;')
     gacha = c.fetchall()
     gacha = random.choice(gacha)
-    kekka = gacha[0]
-    hitokoto = gacha[1]
+    result = gacha[0]
+    word = gacha[1]
     link = gacha[2]
     c.close()
-    return render_template("gacha.html", kekka = kekka, hitokoto = hitokoto, link = link)
+    return render_template("gacha.html", gacha = gacha, result = result, word = word, link = link)
 
 if __name__ == "__main__":
     app.run(debug=True)
