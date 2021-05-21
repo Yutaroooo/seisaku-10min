9ad11f2e5a22b8d28ab7e581ae7acb341452ee48
Yutaro Matsuyama <matsuyamayutaro1@gmail.com>
2021-05-21 14:04:54 +0900

    ガチャ完成

--- app.py
+++ app.py
@@ -1,2 +1,22 @@
-import sqlite3
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
+import sqlite3, random
+
+app = Flask(__name__)
+app.secret_key = "sunabacokoza"
+
+
+@app.route("/gacha")
+def color():
+    conn = sqlite3.connect("gacha.db")
+    c = conn.cursor()
+    c.execute('SELECT "やること", "一言", "リンク" FROM "koumoku-t" WHERE "yaru-id" = 1;')
+    gacha = c.fetchall()
+    gacha = random.choice(gacha)
+    kekka = gacha[0]
+    hitokoto = gacha[1]
+    link = gacha[2]
+    c.close()
+    return render_template("gacha.html", kekka = kekka, hitokoto = hitokoto, link = link)
+
+if __name__ == "__main__":
+    app.run(debug=True)
